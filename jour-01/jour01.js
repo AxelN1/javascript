affc0548c6a43c1e4cdbd6de3036e6a6f36a20b7
Your Name <AxelN1>
2021-03-08 17:10:38 +0100

    message

--- jour-01/jour01.js
+++ jour-01/jour01.js
@@ -1,1 +1,40 @@
-console.log("Hello World !")
+// 01 - Hello World
+
+console.log("Hello World !");
+
+// 02 - String
+
+var test = "My name is Namujimbo";
+console.log(test);
+
+// 03 - Concatenation
+
+var name = "Axel";
+console.log("Nice to meet you" + " " + name);
+
+// 04 - String Length
+
+var testLength = "I'm very long !";
+console.log(testLength.length);
+
+// 05 - Replace
+
+var food = "croissant is meh";
+food = food.replace("meh", "so good");
+console.log(food);
+
+// 06 - Up and Down
+
+var basic = "This is Cool";
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+
+console.log(basic);
+console.log(basicUp);
+console.log(basicDown);
+
+// 07 - Split
+
+var word = "banana";
+var letters = ['b', 'a', 'n', 'a', 'n', 'a'];
+console.log(word)
