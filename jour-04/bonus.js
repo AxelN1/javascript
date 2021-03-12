5e88f21a719eb159ae2eb04a55c86a8f848248c2
Your Name <AxelN1>
2021-03-12 14:06:06 +0100

    jour-04

--- jour-04/bonus.js
+++ jour-04/bonus.js
@@ -10,28 +10,3 @@ return (Math.random(num.length))
 }
 
 generatePassword();
-
-
-/**- Créez une fonction `generatePassword` qui reçoit un paramètre `num`
-- La fonction génère un mot de passe aléatoire, avec autant de lettres que la valeur de `num`
-- Le mot de passe contiendra uniquement des lettres majuscules
-- Si `num` est plus petit que 6 et plus grand que 15, la fonction retourne "error"
-
-Astuce : Vous vous souvenez de la table ASCII ? 😉 */
-
-
-// function randPassword(letters, numbers, either) {
-//     var chars = [
-//      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", // letters
-//      "0123456789", // numbers
-//      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" // either
-//     ];
-  
-//     return [letters, numbers, either].map(function(len, i) {
-//       return Array(len).fill(chars[i]).map(function(x) {
-//         return x[Math.floor(Math.random() * x.length)];
-//       }).join('');
-//     }).concat().join('').split('').sort(function(){
-//       return 0.5-Math.random();
-//     }).join('')
-//   }
