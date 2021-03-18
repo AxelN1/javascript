f721ce9abc508fa8e3d9210ff63ed4d864591d53
AxelN1 <anamujimbo@gmail.com>
2021-03-18 09:24:52 +0100

    jour-05 Node

--- jour-05/guess_04.js
+++ jour-05/guess_04.js
@@ -1,1 +1,22 @@
-mysteryNum = 8;
+var numStart = 1;
+var numEnd = 100;
+var playerNum = 0;
+
+var prompt = require("prompt");
+
+prompt.start();
+
+prompt.get(["Quel est le nombre le mystère ?"], function (err, res) {
+    if (err) {
+        return onerror(err);    
+    }
+
+    var playerNum = res;
+    
+    if (isNaN(playerNum['Quel est le nombre mystère ?'])) { 
+        console.log("Error1", playerNum);           
+    } else {
+        play(numStart, numEnd);     
+    }
+
+});
