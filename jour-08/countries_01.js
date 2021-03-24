d324ccb10bea66a6906a934eafbbde54c6eaadb3
Axel <anamujimbo@gmail.com>
2021-03-24 17:30:34 +0100

    jour-08

--- jour-08/countries_01.js
+++ jour-08/countries_01.js
@@ -1,8 +1,3 @@
-// À l'aide de l'url suivante : "https://restcountries.eu/rest/v1/all" :
-// - Créez un array vide `countriesNames`
-// - Créez une fonction asynchrone `getCountries` pour récupérer les pays et stocker leurs noms dans l'array
-// - Afficher les noms des pays récupérés dans la console, séparés par un tiret
-
 var request = require("request")
 
 var countriesNames = [];
@@ -11,25 +6,14 @@ request.get("https://restcountries.eu/rest/v1/all", function (err, res, body) {
 
     var getCountries = JSON.parse(body);
     console.log(typeof countries);
-    // function getCountries = () {};
     var names = getCountries.map(function (country) {
         return country.name
     });
 
     console.log(names.join("-"));
 
-    // console.log("country :", countriesNames[0].name);
-
-    // console.log("capital :", result[0].capital);
-
-    // if (body.name) {
-    //     countriesNames = result
-    // console.log("country :", countriesNames);
-    // }
-
 });
 
-// console.log("country :", countriesNames[0].name);
 
 
 
